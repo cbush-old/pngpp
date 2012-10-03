bc30a3691f9edfb93f6e7e568c227d8efd75acc4
Chris Bush <chrbsh@gmail.com>
2012-10-02 23:48:11 -0300

    Update pngtest.cpp

--- pngtest.cpp
+++ pngtest.cpp
@@ -1,7 +1,8 @@
 #include <cctype>
 #include "png.h"
 
-
+// load one or more files, print an ascii representation 
+// and write a blurred version to out.png
 int main(int argc, char* argv[]){
 
   for(int i=1; i < argc; i++){
